fa0db0b8a851daf16cd9283f077767126d9ed955
cdaringe <christopher.dieringer@gmail.com>
2019-02-14 00:17:40 -0800

    feat: also protect against cargo wagons

--- control.lua
+++ control.lua
@@ -1,14 +1,26 @@
-SEARCH_RANGE = 1
+SEARCH_RANGE = 2
 function on_pre_death(evt, cause)
   player = game.players[evt.player_index]
+  local search_area = {
+    {player.position.x - SEARCH_RANGE, player.position.y - SEARCH_RANGE},
+    {player.position.x + SEARCH_RANGE, player.position.y + SEARCH_RANGE}
+  }
   local trains = player.surface.find_entities_filtered
   {
-    area =
-    {{player.position.x - SEARCH_RANGE, player.position.y - SEARCH_RANGE},
-    {player.position.x + SEARCH_RANGE, player.position.y + SEARCH_RANGE}},
+    area = search_area,
     type= "locomotive"
   }
-  if #trains > 0 then
+  local cargo_wagons = player.surface.find_entities_filtered
+  {
+    area = search_area,
+    type= "cargo-wagon"
+  }
+  local fluid_wagons = player.surface.find_entities_filtered
+  {
+    area = search_area,
+    type= "fluid-wagon"
+  }
+  if #trains > 0 or #cargo_wagons > 0 or #fluid_wagons > 0 then
     player.character.health = 1
   end
 end
