151d8c8b1cae619fe3aa199d392c1ea77c0c6b64
David La Chasse <david.lachasse@gmail.com>
2019-08-17 20:45:51 -0700

    refactor: consolidate vehicle logic

--- control.lua
+++ control.lua
@@ -5,22 +5,12 @@ function on_pre_death(evt, cause)
     {player.position.x - SEARCH_RANGE, player.position.y - SEARCH_RANGE},
     {player.position.x + SEARCH_RANGE, player.position.y + SEARCH_RANGE}
   }
-  local trains = player.surface.find_entities_filtered
+  local death_machines = player.surface.find_entities_filtered
   {
     area = search_area,
-    type= "locomotive"
+    type= {"locomotive", "cargo-wagon", "fluid-wagon"}
   }
-  local cargo_wagons = player.surface.find_entities_filtered
-  {
-    area = search_area,
-    type= "cargo-wagon"
-  }
-  local fluid_wagons = player.surface.find_entities_filtered
-  {
-    area = search_area,
-    type= "fluid-wagon"
-  }
-  if #trains > 0 or #cargo_wagons > 0 or #fluid_wagons > 0 then
+  if #death_machines > 0 then
     player.character.health = 1
   end
 end
