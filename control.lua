bc00465452b8d8aafa635b1b3f152835ae7b40bc
undermark5 <31358071+undermark5@users.noreply.github.com>
2020-05-22 18:08:49 -0500

    Death Cause Logic Change
    Changed logic for determining cause of death to eliminate ability to abuse camping near train to avoid death.

--- control.lua
+++ control.lua
@@ -1,17 +1,11 @@
-SEARCH_RANGE = 2
-function on_pre_death(evt, cause)
+function on_pre_death(evt)
   player = game.players[evt.player_index]
-  local search_area = {
-    {player.position.x - SEARCH_RANGE, player.position.y - SEARCH_RANGE},
-    {player.position.x + SEARCH_RANGE, player.position.y + SEARCH_RANGE}
-  }
-  local death_machines = player.surface.find_entities_filtered
-  {
-    area = search_area,
-    type= {"locomotive", "cargo-wagon", "fluid-wagon"}
-  }
-  if #death_machines > 0 then
-    player.character.health = 1
+  player_force = player.force
+  if evt.cause.train ~= nil then
+	train_force = evt.cause.force
+	if player_force.index == train_force.index then
+	  player.character.health = 1
+	end
   end
 end
 script.on_event(defines.events.on_pre_player_died, on_pre_death)
