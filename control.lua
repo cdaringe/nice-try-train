2e169d6ccf0224d9683ac2f63faacf9be7d4ae8d
undermark5 <31358071+undermark5@users.noreply.github.com>
2020-05-24 15:37:50 -0500

    Initial Setup
    Added setting to control which trains are dangerous.

--- control.lua
+++ control.lua
@@ -1,11 +1,17 @@
 function on_pre_death(evt)
-  player = game.players[evt.player_index]
-  player_force = player.force
-  if evt.cause.train ~= nil then
-	train_force = evt.cause.force
-	if player_force.index == train_force.index then
-	  player.character.health = 1
-	end
-  end
+    if evt.cause.train ~= nil then
+        setting = settings.global["nice-try-train-friendly-setting"]
+        player = game.players[evt.player_index]
+        player_force = player.force
+        train_force = evt.cause.force
+        if setting == "nice-try-train-my-trains" and player_force.index == train_force.index then
+            player.character.health = 1
+        elseif setting == "nice-try-train-all-trains" then
+            player.character.health = 1
+        elseif setting == "nice-try-train-friend-trains" and train_force.get_friend(player_force) then
+            player.character.health = 1
+        end
+    end
 end
+
 script.on_event(defines.events.on_pre_player_died, on_pre_death)
---
+++ settings.lua
@@ -0,0 +1,13 @@
+data:extend({
+    {
+        type = "string-setting",
+        name = "nice-try-train-friendly-setting",
+        default_value = "my-trains",
+        allowed_values = {
+            "my-trains",
+            "all-trains",
+            "friend-trains"
+        },
+        setting_type = "runtime-global"
+    }
+})
