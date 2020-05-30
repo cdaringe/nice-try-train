cb86e42580899887d4d22836d6e13fed88556615
undermark5 <31358071+undermark5@users.noreply.github.com>
2020-05-30 16:04:13 -0500

    NPE bugfix
    Fixed a NPE that was caused when there was no event associated with death
    eg killed by venom on ground without associated spitter

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
 function on_pre_death(evt)
   player = game.players[evt.player_index]
   player_force = player.force
-  if evt.cause.train ~= nil then
+  if evt.cause ~= nil and evt.cause.train ~= nil then
 	train_force = evt.cause.force
 	if player_force.index == train_force.index then
 	  player.character.health = 1
