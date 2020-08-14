079cccb73507dac48c8fda689a7b8faebe0166b6
Nathan <nsshurtz@gmail.com>
2020-08-14 16:55:30 -0500

    fixed settings ready for relase

--- control.lua
+++ control.lua
@@ -1,14 +1,15 @@
 function on_pre_death(evt)
     if evt.cause ~= nil and evt.cause.train ~= nil then
-        setting = settings.global["nice-try-train-friendly-setting"]
+        setting = settings.global["nice-try-train-friendly-setting"].value
         player = game.players[evt.player_index]
         player_force = player.force
         train_force = evt.cause.force
-        if setting == "nice-try-train-my-trains" and player_force.index == train_force.index then
+        player.print("setting: " .. setting .. "\nplayer: " .. player.name .. "\nplayer_force: " .. player_force.index .. "\ntrain_force: " .. train_force.index)
+        if setting == "my-trains" and player_force.index == train_force.index then
             player.character.health = 1
-        elseif setting == "nice-try-train-all-trains" then
+        elseif setting == "all-trains" then
             player.character.health = 1
-        elseif setting == "nice-try-train-friend-trains" and train_force.get_friend(player_force) then
+        elseif setting == "friend-trains" and (train_force.get_friend(player_force) or train_force.index == player_force.index)then
             player.character.health = 1
         end
     end
