8303ad18ad93f8b95ce71797866de9ca8ae431fd
Nathan Shurtz <nsshurtz@gmail.com>
2021-04-27 13:39:02 -0500

    fix compatibility with Space Exploration

--- control.lua
+++ control.lua
@@ -1,17 +1,13 @@
-function on_pre_death(evt)
-    if evt.cause ~= nil and evt.cause.train ~= nil then
-        setting = settings.global["nice-try-train-friendly-setting"].value
-        player = game.players[evt.player_index]
-        player_force = player.force
-        train_force = evt.cause.force
-        if setting == "my-trains" and player_force.index == train_force.index then
-            player.character.health = 1
-        elseif setting == "all-trains" then
-            player.character.health = 1
-        elseif setting == "friend-trains" and (train_force.get_friend(player_force) or train_force.index == player_force.index)then
-            player.character.health = 1
+function damaged(evt)
+    if evt.entity ~= nil and evt.entity.type == "character" and evt.cause ~= nil and evt.cause.train ~= nil then
+        local setting = settings.global["nice-try-train-friendly-setting"].value
+        local damaged_force = evt.entity.force
+        local cause_force = evt.cause.force
+        if (setting == "my-trains" and damaged_force.index == cause_force.index) or setting == "all-trains" or (setting == "friend-trains" and (cause_force.get_friend(damaged_force) or cause_force.index == damaged_force.index)) then
+            evt.final_health = 1
+            evt.entity.health = 1
         end
     end
 end
 
-script.on_event(defines.events.on_pre_player_died, on_pre_death)
+script.on_event(defines.events.on_entity_damaged, damaged, {{filter="damage-type", type="impact"},{mode="and", filter="final-health", value=0, comparison="="}})
